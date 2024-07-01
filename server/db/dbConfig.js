7df73b441a85121a17e3390886212b1317ed47c2
Ydidiya Tarekegn <Ydidiyatare7@gmail.com>
2024-06-30 22:18:09 -0700

    Fix: Database environment var

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -1,4 +1,4 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
 // const pool = new Pool({
 //   connectionString: process.env.POSTGRES_URL,
@@ -7,14 +7,31 @@ const { Pool } = require("pg");
 //   }
 // });
  
-const pool = new Pool({
-  user: 'postgres',      // Your database username
-  host: 'localhost',
-  database: 'postgres',  // Your database name
-  password: 'Brookspsql1996',  // Your database password
-  port: 5432,                // Your database port
-});
+// const pool = new Pool({
+//   user: 'postgres',      // Your database username
+//   host: 'localhost',
+//   database: 'postgres',  // Your database name
+//   password: 'Ydidiya@777',  // Your database password
+//   port: 5432,                // Your database port
+// });
 
+
+// pool.connect((err) => {
+//   if (err) throw err;
+//   console.log("Connect to PostgreSQL successfully!");
+// });
+
+// module.exports = pool;
+
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 pool.connect((err) => {
   if (err) throw err;
