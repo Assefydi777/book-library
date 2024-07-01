4d25cd2a65e0473763a7c9af18bae608b1a2b208
Ydidiya Tarekegn <Ydidiyatare7@gmail.com>
2024-06-30 22:52:28 -0700

    Fix: axios library to hit postgres

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5500/api/',
+    baseURL: 'https://book-library-nd76.onrender.com/api/',
     headers: {
         'Content-Type': 'application/json'
     }
